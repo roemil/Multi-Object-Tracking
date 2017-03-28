8a988672fb559df26ce60d92f5ac9a7820f47304
Joachim <jocke.benjaminsson@hotmail.com>
2017-03-28 09:51:52 +0200

    Updated PMBM

--- src/PMBM.m
+++ src/PMBM.m
@@ -1,7 +1,5 @@
 %%%%% PMBM %%%%%
-
-% Measurements
-Z = cell(1);
+function [Xpred, Xupd] = PMBM(Z)
 
 % Inititate
 sigmaQ = 1;         % Process (motion) noise
@@ -29,15 +27,18 @@ XuUpd = cell(1);
 % Initiate pot new
 %XmuPred = cell(1,1,4);
 %XmuUpd = cell(1,1,4);
-XmuPred = cell(1);
-XmuUpd = cell(1);
+XmuPred = cell(1); % XmuPred{t}(i)
+XmuUpd = cell(1,1); % XmuUpd{t,z}(i)
 % Xmu = [wu, state, Pu, S]
 
-% Initiate potential targets. X = {t,j,i,x}
+% Inititate hypotheses
+Xhypo = cell(1,1); % Xhypo{t,z}(i)
+
+% Initiate potential targets. X = {t,j}(i)
 %Xpred = cell(1,1,1,5);
 %Xupd = cell(1,1,1,5);
-Xpred = cell(1);
-Xpred(1);
+Xpred = cell(1,1);
+Xupd = cell(1,1);
 % x = [w, state, P, r, z]'
 
 % Generate motion and measurement models
@@ -52,49 +53,72 @@ for k = 2:K % For each time step
     
     % Poisson - Assume constant birth atm
     for i = 1:size(XuUpd{k-1},2) % For each single target component i
-    [XmuPred{k}(i).state, XmuPred{k}(i).Pu] = KFPred(XuUpd{k-1}(i).state,F, XuUpd{k-1}(i).Pu, Q);
+        [XmuPred{k}(i).state, XmuPred{k}(i).P] = KFPred(XuUpd{k-1}(i).state,F, XuUpd{k-1}(i).P, Q);
      
 %     XmuPred{k,1} = [wb{1} XuUpd{k-1,1}];    % Pred weight
 %     XmuPred{k,2} = [Xb{1} XuPred{k,2}];      % Pred state
 %     XmuPred{k,3} = [Pb{1} XuPred{k,3}];      % Pred cov
         XmuPred{k}(i).w = [wb{1} XuUpd{k-1}(i).w];    % Pred weight
         XmuPred{k}(i).state = [Xb{1} XuPred{k}.state];      % Pred state
-        XmuPred{k}(i).Pu = [Pb{1} XuPred{k}(i).Pu];      % Pred cov    
-        mu = @(x) gaussMix(XuPred{k}(i).w,XuPred{k}(i).state,XuPred{k}(i).Pu); % Pred Poisson intensity
+        XmuPred{k}(i).P = [Pb{1} XuPred{k}(i).P];      % Pred cov    
+        mu = @(x) gaussMix(XuPred{k}(i).w,XuPred{k}(i).state,XuPred{k}(i).P); % Pred Poisson intensity
         
         % MIGHT HAVE MIXED UP PREDICTION STEPS
-    
-        % Bernoulli
-        Xpred{k}(i).w = Xupd{k-1}(i).w;      % Pred weight
-        [Xpred{k}(i).state, Xpred{k}(i).state] = KFPred(Xupd{k-1}(i).state, F, Xupd{k-1}(i).Pu ,Q);    % Pred state
-        Xpred{k}(i).r = Ps*Xupd{k-1}(i).r;   % Pred prob. of existence
+        
+        % Update undetected targets (Poisson component)
+        XuUpd{k}(i).w = (1-Pd)*XmuPred{k}(i).w;
+        XuUpd{k}(i).state = XmuPred{k}(i).state;
+        XuUpd{k}(i).P = XmuPred{k}(i).P;
     end
+    
+    for j = 1:size(Xupd{k},2)
+        for i = 1:size(Xupd{k,j},2)
+            % Bernoulli
+            Xpred{k,j}(i).w = Xupd{k-1,j}(i).w;      % Pred weight
+            [Xpred{k,j}(i).state, Xpred{k,j}(i).P] = KFPred(Xupd{k-1,j}(i).state, F, Xupd{k-1,j}(i).P ,Q);    % Pred state
+            Xpred{k,j}(i).r = Ps*Xupd{k-1,j}(i).r;   % Pred prob. of existence
+        end
+    end
+
     %%%%% Update %%%%%
 
-    % Update undetected targets (Poisson component)
-    XuUpd{k,1} = (1-Pd)*XmuPred{k,1};
-    XuUpd{k,2} = XmuPred{k,2};
-    XuUpd{k,3} = XmuPred{k,3};
-
-    S = 0;
-    w = zeros(1,size(Z{k},2));
     % Update for potential targets detected for the first time
     for z = 1:size(Z{k},2)
+        w = zeros(1,size(Z{k},2));
+        Xmutmp = zeros(4,size(XmuPred{k,2},2));
+        Stmp = cell(size(XmuPred{k,2},2));
         for i = 1:size(XmuPred{k,2},2)
             % Pass through Kalman
-            [XmuUpd{k,2}, XmuUpd{k,3}, XmuUpd{k,4}] = KFUpd(XmuPred{k,2},H, XmuPred{k,3}, R, Z{k}(:,z));
+            [XmuUpd{k,z}(i).state, XmuUpd{k,z}(i).P, XmuUpd{k,z}(i).S] = KFUpd(XmuPred{k}(i).state,H, XmuPred{k}(i).P, R, Z{k}(:,z));
             
             % TODO: DEFINE THESE AS FUNCTIONS AND JUST PASS DIFF z? 
             % Compute weight
-            w(1,i) = XmuPred{k,1}(:,i)*mvnpdf(Z{k}(:,z), H*XmuPred{k,2}, S); 
-
-            e = Pd*generateGaussianMix(Z{k}(:,z), H*XmuPred{k,2}, S);
-        rho = e+c;
-        r = e/rho;
+            w(1,i) = XmuPred{k}(i).w*mvnpdf(Z{k}(:,z), H*XmuPred{k}(i).state, XmuUpd{k,z}(i).S);
+            
+            % TODO: temp solution
+            Xmutmp(1:4,i) = XmuPred{k}(i).state;
+            Stmp{i} = XmuUpd{k,z}(i).S;
+        end
+        % Normalize weight
+        w = w/sum(w);
         
-        
-        
+        e = Pd*generateGaussianMix(Z{k}(:,z), H*Xmutmp, Stmp);
+        XmuUpd{k,z}.w = e+c; % rho
+        XmuUpd{k,z}.r = e/XmuUpd{k,z}.w;
+    end
+    
+    % Create missdetection hypo in index size(Z{k},2)+1
+    for j = 1:size(Xpred{k},2)
+        for i = 1:size(Xpred{k,j},2)
+            Xhypo{k,size(Z{k},2)+1}(i).w = Xpred{k,j}(i).w*(1-Xpred{k,j}(i).r+Xpred{k,j}(i).r*(1-Pd));
+            Xhypo{k,size(Z{k},2)+1}(i).r = Xpred{k,j}(i).r*(1-Pd)/(1-Xpred{k,j}(i).r+Xpred{k,j}(i).r*(1-Pd));
+            Xhypo{k,size(Z{k},2)+1}(i).state = Xpred{k,j}(i).state;
+            Xhypo{k,size(Z{k},2)+1}(i).P = Xpred{k,j}(i).P;
+        end
+    end
+            
     % Update for previously potentially detected targets
-
-
+    for z = 1:size(Z{k},2)
+        for j = 1:size(Xpred{k},2)
+            for i = 1:size(Xpred{k,j},2)
 end
--- src/generateGaussianMix.m
+++ src/generateGaussianMix.m
@@ -2,6 +2,12 @@ function gaussMix = generateGaussianMix(w, mu, sigma)
 
 gaussMix = @(x) 0;
 
-for i = 1:size(w,2)
-    gaussMix = @(x) gaussMix(x)+w(i)*mvnpdf(x, mu(i), sigma(i));
+if iscell(sigma)
+    for i = 1:size(w,2)
+        gaussMix = @(x) gaussMix(x)+w(i)*mvnpdf(x, mu(:,i), sigma{i});
+    end
+else
+    for i = 1:size(w,2)
+        gaussMix = @(x) gaussMix(x)+w(i)*mvnpdf(x, mu(i), sigma(i));
+    end
 end
