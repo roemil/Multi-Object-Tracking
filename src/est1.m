729c5d8f8463dc2a28bd7c856bb6e25d55572335
rosenber <emil.rosenberg@eurecom.fr>
2017-03-30 14:10:13 +0200

    wrote code for estimator 1. Finished

--- src/est1.m
+++ src/est1.m
@@ -1,4 +1,33 @@
-function Xest = est1(Xupd)
+% Function to estimate states in PMBM
+% Input: Xupd is global hypotheses at all times up to time k
+%        Threshold is the parameter that determines which components is
+%        alive. 
+% Output: Most likely global hypothesis at time k
+%
+%
+%
+%
+%
 
-for j = 1:size(Xupd,2)
-    for i = 1:size(Xupd{j})
+
+function Xest = est1(Xupd, threshold)
+M = 0;
+prod = 1;
+for j = 1:size(Xupd{end},2)
+    for i = 1 : size(Xupd{end,j},2) % find index of which global hyp is 
+        prod= prod*Xupd{end,j}(i).w;% most likely
+    end
+    if(prod >= M)
+        ind = j;
+        M = prod;
+    end 
+end
+index = 1;
+for i = 1 : size(Xupd{end,ind},2)
+    if(Xupd{end,ind}(i).r > threshold) % if prob. of existence great enough
+        Xupd{end,ind}(i).r
+        Xest{index} = Xupd{end,ind}(i).state; % store mean (i.e states)
+        index = index + 1;
+    end
+end
+end
