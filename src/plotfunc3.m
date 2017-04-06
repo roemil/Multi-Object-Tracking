0d732a7822feb8e814f28bee983d9a8e52a3be5b
rosenber <emil.rosenberg@eurecom.fr>
2017-04-06 16:16:17 +0200

    murty

--- src/plotfunc3.m
+++ src/plotfunc3.m
@@ -4,25 +4,42 @@ N = 65; %size(Z,2);
 %t = cell(1,2);
 t1 = zeros(4,N);
 t2 = zeros(4,N);
+tvec = cell(1);
+tvec{1} = t1;
+tvec{2} = t2;
 Xtrue1 = zeros(4,N);
 Xtrue2 = zeros(4,N);
 Z1 = zeros(2,N);
 Z2 = zeros(2,N);
 for k = 2:N %size(Z,2)
-    x1diff = Xest{k}{1}(1)-Xtrue{k}(1,1);
-    y1diff = Xest{k}{1}(2)-Xtrue{k}(2,1);
-    x2diff = Xest{k}{2}(1)-Xtrue{k}(1,1);
-    y2diff = Xest{k}{2}(2)-Xtrue{k}(2,1);
-    d1 = sqrt(x1diff^2+y1diff^2);
-    d2 = sqrt(x2diff^2+y2diff^2);
-    if d1 < d2
-        t1(:,k) = Xest{k}{1};
-        t2(:,k) = Xest{k}{2};
-        
-    else
-        t1(:,k) = Xest{k}{2};
-        t2(:,k) = Xest{k}{1};
+    
+    
+    for i = 1 : size(Xest{k},2)
+        xdiff(i) = calcdiff(Xest{k}{i},Xtrue{k}(:,i),'x');
+        ydiff(i) = calcdiff(Xest{k}{i},Xtrue{k}(:,i),'y');
+        d(i) = calcdist(xdiff(i),ydiff(i)); 
     end
+    dtmp = d;
+    [~,ind] = min(dtmp);
+    indvec = ind;
+    for i = 1 : size(Xest{k},2)
+        dtmp(ind) = 100;
+        [~,ind]= min(dtmp);
+        indvec = [indvec, ind];
+    end
+%     d_sorted = sort(d);
+%     for i = 1 : size(Xest{k},2)
+%         tvec{i}(:,k) = Xest{k}{indvec(i)};
+%             
+%     end
+%     if d1 < d2
+%         t1(:,k) = Xest{k}{1};
+%         t2(:,k) = Xest{k}{2};
+%         
+%     else
+%         t1(:,k) = Xest{k}{2};
+%         t2(:,k) = Xest{k}{1};
+%     end
     Xtrue1(:,k) = Xtrue{k}(1:4,1);
     Xtrue2(:,k) = Xtrue{k}(1:4,2);
     Z1(:,k) = Z{k}(:,1);
@@ -37,7 +54,7 @@ figure(fig1)
 subplot(4,1,1)
 plot(K,Xtrue1(1,2:end),'-*k')
 hold on
-plot(K,t1(1,2:end),'*r')
+plot(K,tvec{1}(1,2:end),'*r')
 plot(K,Z1(1,2:end),'ob')
 title('Position')
 ylabel('')
@@ -46,20 +63,20 @@ figure(fig1)
 subplot(4,1,2)
 plot(K,Xtrue1(2,2:end),'*k')
 hold on
-plot(K,t1(2,2:end),'*r')
+plot(K,tvec{1}(2,2:end),'*r')
 plot(K,Z1(2,2:end),'ob')
 
 figure(fig1)
 subplot(4,1,3)
 plot(K,Xtrue1(3,2:end),'*k')
 hold on
-plot(K,t1(3,2:end),'*r')
+plot(K,tvec{1}(3,2:end),'*r')
 title('Velocity')
 figure(fig1)
 subplot(4,1,4)
 plot(K,Xtrue1(4,2:end),'*k')
 hold on
-plot(K,t1(4,2:end),'*r')
+plot(K,tvec{1}(4,2:end),'*r')
 
 fig2 = figure;
 
