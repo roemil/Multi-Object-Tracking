a126ac40b94ec08d515b13e49aa7a3d9ed4450be
rosenber <emil.rosenberg@eurecom.fr>
2017-04-10 16:33:31 +0200

    fix

--- src/main.m
+++ src/main.m
@@ -3,10 +3,12 @@ load('case3');
 ind = 1;
 for i = 1 : size(Z,2)
     if(~isempty(Z{i}))
-        Z{i} = Z{i};
+        Z{ind} = Z{i};
         ind = ind + 1;
     end
 end
 
-[pred,upd,est]=PMBM(Z);
+[pred,upd,est, Pest]=PMBM(Z);
 
+%%
+plotStates(Z,X,est,Pest)
