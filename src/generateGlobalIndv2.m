0ac9bec439a79601ef9e06b76ada5672f66c5270
Joachim <jocke.benjaminsson@hotmail.com>
2017-04-20 10:31:43 +0200

    Improved efficiency generateGlobalIndv2

--- src/generateGlobalIndv2.m
+++ src/generateGlobalIndv2.m
@@ -6,25 +6,66 @@ vec = [1:nbrOldTargets, (nbrOldTargets+1)*ones(1,m)];
 t1 = nchoosek(vec, m);
 t2 = unique(t1,'rows');
 
-Amat = [];
+% t2length = 0;
+% for i = 0:min(m,nbrOldTargets)
+%     t2length = t2length + nchoosek(nbrOldTargets,i);
+% end
+
+% AmatLength = zeros(min(m,nbrOldTargets),1);
+% for i = 0:min(m,nbrOldTargets)
+%     AmatLength(i+1) = nchoosek(m,i)*factorial(i)*nchoosek(nbrOldTargets,i);
+% end
+
+oldTargets = 1:nbrOldTargets;
+
+AmatLength = zeros(size(t2,1),1);
 for i = 1:size(t2,1)
-    tmp1 = perms(t2(i,:));
-    tmp2 = unique(tmp1,'rows');
+    nbrTargets = sum(ismember(oldTargets,t2(i,:)));
+    AmatLength(i) = nchoosek(m,nbrTargets)*factorial(nbrTargets);
+end
+indeces = cumsum(AmatLength);
+
+nbrHypo = sum(AmatLength);
+Amat = zeros(nbrHypo,m);
+oldInd = [1; indeces(1:end-1)+1];
+for i = 1:size(t2,1)
+    tmp2 = uniqperm(t2(i,:));
+%     tmp1 = perms(t2(i,:));
+%     tmp2 = unique(tmp1,'rows');
+    % FOR CHECK, why perms give multiple of the same?
+    %sum(ismember(tmp1,tmp2,'rows')) == size(tmp1,1);
+%     if size(tmp1,1) ~= size(tmp2,1)
+%         keyboard
+%     end
+    % FOR CHECK
     [~, cols] = find(tmp2 == (nbrOldTargets+1));
     ind = find(tmp2 == (nbrOldTargets+1));
     if ~isempty(cols)
         tmp2(ind) = cols+nbrOldTargets;
     end
-    Amat = [Amat; tmp2];
+    Amat(oldInd(i):indeces(i),:) = tmp2;
 end
 
-nbrHypo = size(Amat,1);
 S = zeros(m, m+nbrOldTargets, nbrHypo);
-for i = 1:nbrHypo
-    for z = 1:m
-        S(z, Amat(i,z), i) = 1;
-    end
-end
+measVec = (1:m)';
+AmatSize = size(Amat);
+
+%indNbr = reshape(1:prod(AmatSize),m,nbrHypo)';
+indNbr = reshape(1:numel(Amat),nbrHypo,m)'; % Numel faster?
+
+indVec = sub2ind(size(S(:,:,1)),repmat(measVec,1,nbrHypo),Amat(indNbr));
+nbrElementsS = m*(m+nbrOldTargets);
+addVec = 1:nbrHypo-1;
+indVec = [indVec(:,1), nbrElementsS*addVec+indVec(:,2:end)];
+
+S(indVec) = 1;
+
+%%%%% OLD %%%%%
+% for i = 1:nbrHypo
+%     for z = 1:m
+%        S(z, Amat(i,z), i) = 1;
+%     end
+% end
 
 % indOld = 1;
 % nbrHypo = size(Amat2,1);
