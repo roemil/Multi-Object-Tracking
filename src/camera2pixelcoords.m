4ae1fe2a1398b71d997e0418c5681b47cc0d8796
rosenber <emil.rosenberg@eurecom.fr>
2017-04-21 08:49:09 +0200

    we now have GT data

--- src/camera2pixelcoords.m
+++ src/camera2pixelcoords.m
@@ -3,17 +3,12 @@
 %   output: 2D coordinates in image plane
 %
 function x = camera2pixelcoords(X,P)
-  % project in image
-  if(~isempty(X{1}))
-      for i = 1 : size(X,2)
-        x{i} = P * [X{i}(1:3); 1];
-      % scale projected points
-        x{i}(1,:) = x{i}(1,:)./x{i}(3,:);
-        x{i}(2,:) = x{i}(2,:)./x{i}(3,:);
-        %x{i}(3,:) = [];
-        x{i}(4:6) = X{i}(4:end);
-      end
-  else
-      x = cell(1);
+% project in image
+  for i = 1 : size(X,2)
+    x = P * [X(1:3); 1];
+  % scale projected points
+    x(1,:) = x(1,:)./x(3,:);
+    x(2,:) = x(2,:)./x(3,:);
+    x(3,:) = [];
   end
 end
