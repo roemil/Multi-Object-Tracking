2f9f95a83291268bd2513aaca196d08b3f2e3aab
rosenber <emil.rosenberg@eurecom.fr>
2017-04-24 09:59:33 +0200

    cleaned up code

--- src/plotGT.m
+++ src/plotGT.m
@@ -39,25 +39,19 @@ fclose(f);
 % Find indices for the frame number of interest
 ind = find(ismember(dataArray{1},frameNum,'rows') == 1 & ismember(dataArray{2},-1,'rows') == 0);
 
-xInd = 14;
-yInd = 15;
-zInd = 16;
+
 % Find bounding box data
-pixcoords = zeros(2,length(ind));
+width = zeros(1,length(ind));
+height = zeros(1,length(ind));
+boxes = zeros(length(ind),4);
 for i = 1 : length(ind)
-    %xcoord1 = dataArray{xInd}(ind(i));
-    %ycoord1 = dataArray{yInd}(ind(i));
-    %zcoord1 = dataArray{zInd}(ind(i));
-    %pixcoords(:,i) = camera2pixelcoords([xcoord1;ycoord1;zcoord1],P);
-    %xcoord(i) = pixcoords(1,i);
-    %ycoord(i) = pixcoords(2,i);
     width(i) = dataArray{9}(ind(i))-dataArray{7}(ind(i))+1;
     height(i) = dataArray{10}(ind(i))-dataArray{8}(ind(i))+1;
-    %boxes(i,:) = [xcoord(i)-width(i)/2, ycoord(i)-height(i), width(i), height(i)]';
     boxes(i,:) = [dataArray{7}(ind(i)),dataArray{8}(ind(i)), width(i), height(i)]';
 end
     % Store data in format for rectangle-function
-
+xcoord = zeros(1,size(GT,2));
+ycoord = zeros(1,size(GT,2));
 for i = 1 : size(GT,2)
     xcoord(i) = GT(1,i);
     ycoord(i) = GT(2,i);
@@ -75,16 +69,3 @@ for i = 1:size(ind,1)
 end
 plot(xcoord,ycoord,'rx')
 
-% maxWidth = max(boxes(:,3));
-% maxHeight = max(boxes(:,4));
-% 
-% if ~isempty(Xest{1})
-%     for i = 1:size(Xest,2)
-%         if size(Xest{i},1) == 4
-%             Xest{i}(5) = maxWidth;
-%             Xest{i}(6) = maxHeight;
-%         end
-%         estBox = [Xest{i}(1)-Xest{i}(5)/2, Xest{i}(2)-Xest{i}(6)/2, Xest{i}(5), Xest{i}(6)];
-%         rectangle('Position',estBox,'EdgeColor','r','LineWidth',1)
-%     end
-% end
