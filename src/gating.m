37dcce44c27f81e7e019cab9f3733bd2289435a3
rosenber <emil.rosenberg@eurecom.fr>
2017-05-02 15:14:56 +0200

    gating func

--- src/gating.m
+++ src/gating.m
@@ -1,11 +1,12 @@
 %%% Ellipsoidal gating
 %%% Input: Measurement Z, Measurement Model H, Predicted states Xest
 %%% Output: Subset Z
-function [ind] = gating(Z, H, P, X,threshold)
-eps = [];
+function [ind] = gating(Z, H, X, R, threshold)
+eps = zeros(2,size(X,2));
+d = zeros(1,size(X,2));
 for i = 1 : size(X,2)
-    eps(:,i) = Z - H*X(i).state(1:4,i); % Innovation
-    S = H*P*H'+R; % create inovation variance matrix S
+    eps(:,i) = Z(1:2) - H*X(i).state(1:4); % Innovation
+    S = H*X(i).P*H'+R; % create inovation variance matrix S
     d(i) = eps(:,i)'/S*eps(:,i); % Mahalanobis distance
 end
 
