5b403e1ba75775a65579c355bf7c3f30a66e9c5a
rosenber <emil.rosenberg@eurecom.fr>
2017-05-23 16:48:28 +0200

    fix color

--- src_3states/colorcomp.m
+++ src_3states/colorcomp.m
@@ -1,8 +1,8 @@
-function davg = colorcomp(red1,green1,blue1, red2, green2, blue2)
+function davg = colorcomp(red1, green1, blue1, red2, green2, blue2)
 
 dred = chisdist(red1,red2);
 dgreen = chisdist(green1,green2);
 dblue = chisdist(blue1,blue2);
-davg = (dred+dgreen+dblue)/3;
+davg = mean([dred, dgreen, dblue]);
 
 end
